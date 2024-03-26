6ea1cb91ab4aa73d5b0cbd4749c56b9b2ed62972
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-26 18:04:05 +0500

    import dependecies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react'
 import './App.css'
+import * as tf from '@tensorflow/tfjs'
+import * as bodyPix from '@tensorflow-models/body-pix'
+import Webcam from 'react-webcam'
 
 export default function App() {
 
@@ -10,7 +13,7 @@ export default function App() {
     <>
       <div className="App">
         <header className="App-header">
-       
+      
         </header>
       </div>
     </>
