cff5266f1e1eaca65a63e0215b46e5e91efbd3a6
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-26 18:05:05 +0500

    setup webcam  and canvas

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,35 @@ export default function App() {
     <>
       <div className="App">
         <header className="App-header">
-      
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
+
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
         </header>
       </div>
     </>
