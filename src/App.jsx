d1105c66bd4df69eb2cb08e3f470b6430a52e6d4
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-26 18:02:15 +0500

    install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react'
+import './App.css'
+
+export default function App() {
+
+  const webcamRef  = useRef(null)
+  const canvasRef = useRef(null)
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+       
+        </header>
+      </div>
+    </>
+  )
+}
