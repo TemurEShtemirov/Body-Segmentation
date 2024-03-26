ad82b9a0a5ee410b79086055d9673becf13bb123
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-26 18:17:36 +0500

    detect function

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,40 @@ export default function App() {
   const runBodysegment = async () => {
     const net = await bodyPix.load()
     console.log("Bodypix model loaded")
+
+    setInterval(() => {
+      detect(net)
+    }, 10)
+  }
+
+
+  const detect = async (net) => {
+    // Check data is available
+    if (typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get video properties
+      const video = webcamRef.current.video;
+      const videoHeight = video.videoHeight;
+      const videoWidth = video.videoWidth;
+
+      // Set video width and height
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // Set canvas width and height
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      // Make datections
+      const person = await net.segmentPersonParts(video);
+      console.log(person);
+
+      // Draw detections
+    }
+
+
   }
 
   runBodysegment()
